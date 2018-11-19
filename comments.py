5bf0c6503852f3ab47c455736071745ec41402db
Harrison-Gitau <gitau.h.mwangi@gmail.com>
2018-11-19 11:49:10 +0300

    [Feature #162043831] Add empty list for database

---
+++ comments.py
@@ -0,0 +1,18 @@
+comments = []
+
+class Comments:
+
+	def __init__(self):
+		self.db = comments
+
+
+	def create_comments(self, message, timestamp, author):
+
+		payload = {
+			"id": len(self.db)+1,
+			"message": message,
+			"timestamp": timestamp,
+			"author": author
+		}
+		self.db.append(payload)
+		return payload
